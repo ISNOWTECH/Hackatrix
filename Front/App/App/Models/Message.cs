3a4e676ff18dd491688f1862ccddc09400d27c3e
Spectralbolt <37422018+Rattenfengar@users.noreply.github.com>
2019-09-28 11:56:22 -0500

    Messages services added

--- Front/App/App/Models/Message.cs
+++ Front/App/App/Models/Message.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace App.Models
 {
     public class Message
     {
         public int Id { get; set; }
+        public string Sender { get; set; }
         public string Msg { get; set; }
+        public LayoutOptions Align { get; set; }
         public DateTime Date { get; set; }
+        public Color BgColor { get; set; }
     }
 }
---
+++ Front/App/App/ViewModels/ChatViewModel.cs
@@ -0,0 +1,53 @@
+﻿using App.Models;
+using App.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App.ViewModels
+{
+    public class ChatViewModel:BaseViewModel
+    {
+        private readonly IService service;
+
+        public ObservableCollection<Message> Messages { get; set; }
+        public ICommand RefreshMessages { get; set; }
+        public ChatViewModel(IService service)
+        {
+            this.service = service;
+            RefreshMessages = new Command(async () => await SyncMessages());
+            Device.StartTimer(TimeSpan.FromMilliseconds(200), () => 
+            {
+                RefreshMessages.Execute(null);
+                return true;
+            }
+
+            );
+        }
+        private async Task SyncMessages()
+        {
+            var msgs = await service.GetMessages();
+            foreach(var item in msgs)
+            {
+                if (!Messages.Contains(item))
+                {
+                    if (item.Sender.Equals("Driver"))
+                    {
+                        item.Align = LayoutOptions.Start;
+                        item.BgColor = Color.LightBlue;
+                    }
+                    else if (item.Sender.Equals("User"))
+                    {
+                        item.Align = LayoutOptions.End;
+                        item.BgColor = Color.LightGreen;
+                    }
+                    Messages.Add(item);
+                }
+            }
+        }
+    }
+}
